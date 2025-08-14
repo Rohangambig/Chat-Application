e0ca267bdc98812a9aaeca8cc6fa1993ead3f20c
Rohangambig <120889961+Rohangambig@users.noreply.github.com>
2025-08-14 15:23:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const {Server} = require('socket.io');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin:'http://127.0.0.1:5500',
+        methods:['GET',"POST"]
+    }
+});
+
+const userList = new Map();
+const messageList = [];
+
+io.on('connection',(socket) => {
+
+    socket.on('newUser',(username) => {
+        socket.username = username;
+        userList.set(socket.id,username);
+        io.emit('userList',Array.from(userList.values()));
+    })
+
+    socket.on('messageSend',(data) => {
+        messageList.push({name:data.user,message:data.message});
+        io.emit('messageList',Array.from(messageList));
+    })
+
+    socket.on('disconnect',() => {
+        userList.delete(socket.id);
+        io.emit('userList',Array.from(userList.values()));
+    })
+
+})
+
+server.listen(5001,() => {
+    console.log('Server is listening')
+})
